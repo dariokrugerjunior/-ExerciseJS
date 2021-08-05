8e855f41186483479c2f75423426ffe848b11cb2
Dario Kruger Junior <krugerdario1@gmail.com>
2021-08-04 22:02:54 -0300

    criando a inner, para mudar o horario. Imagem alterado dentro do body no html usando o if.

--- script.js
+++ script.js
@@ -2,7 +2,24 @@ function loading() {
 
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('image')
-    var data = new Date()
-    var hora = data.getHours()
+    var date = new Date()
+    var hours = date.getHours()
+    msg.innerHTML = `Agora são ${hours} horas`
+
+    if (hours >= 0 && hours < 12) {
+        // BOM DIA
+        img.src = 'photos/fotomanha.png'
+        document.body.style.background = '#add8e6'
+    } else if (hours >= 12 && hours <= 18) {
+        // BOA TARDE
+        img.src = 'photos/fototarde.png'
+        document.body.style.background = '#ffa500'
+    } else {
+        // BOA NOITE
+        img.src = 'photos/fotonoite.png'
+        document.body.style.background = '#191970'
+    }
+
+
 }
 
