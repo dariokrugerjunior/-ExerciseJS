38a14c10b0a92d8dcc68b5f43536dfca2208520e
Dario Kruger Junior <krugerdario1@gmail.com>
2021-08-04 21:31:16 -0300

    Criando as var para usar a função no body do html

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert('Olá')
+function loading() {
+
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('image')
+    var data = new Date()
+    var hora = data.getHours()
+}
+
